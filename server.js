01897858f480697a3b45e347ff43865bcc89745b
Nils-Morten Nilssen <nmnilssen@gmail.com>
2018-04-30 19:50:59 +0200

    Added method to upload list of products

--- server.js
+++ server.js
@@ -81,50 +81,64 @@ app.post('/collect/:id', function(req, res) {
   }
 
   db.none('INSERT into collected(username, barcode, info, collected) values($1, $2, $3, current_timestamp)', [user, id, info])
-        .then(function() {
-            res.status(200)
-                .json({
-                    status: 'success',
-                    message: 'Inserted new entry in collected table'
-                });
-        })
-        .then(null, function(err) {
-            return next(err);
+    .then(function() {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted new entry in collected table'
         });
+    })
+    .then(null, function(err) {
+      return next(err);
+    });
 });
 
 app.get('/collected', function(req, res, next) {
   db.any('SELECT * FROM collected')
-        .then(function(data) {
-            console.log(data);
-            res.status(200).json(data);
-        })
-        .then(null, function(err) {
-            console.error(err);
-            return next("Error " + err);
-        });
-  });
+    .then(function(data) {
+      console.log(data);
+      res.status(200).json(data);
+    })
+    .then(null, function(err) {
+      console.error(err);
+      return next("Error " + err);
+    });
+});
 
 
 app.get('/products', function(req, res, next) {
-    db.any('SELECT barcode, benevning, vendor, vendoritemnumber FROM products')
-      .then(function(data) {
-          res.status(200).json(data);
-      })
-      .then(null, function(err) {
-          return next(err);
-      });
+  db.any('SELECT barcode, benevning, vendor, vendoritemnumber FROM products')
+    .then(function(data) {
+      res.status(200).json(data);
+    })
+    .then(null, function(err) {
+      return next(err);
+    });
 });
 
 app.get('/products/:id', function(req, res, next) {
   db.any('SELECT * FROM products where barcode=$1', [req.params.id])
-      .then(function(data) {
-          res.status(200).json(data);
-      })
-      .then(null, function(err) {
-          return next(err);
-      });
+    .then(function(data) {
+      res.status(200).json(data);
+    })
+    .then(null, function(err) {
+      return next(err);
+    });
 });
+
+app.post('/products', function(req, res, next) {
+  console.log("body:" + JSON.stringify(req.body));
+  insertList(req.body)
+    .then(function(data) {
+      res.status(200).json({
+        status: 'success',
+        message: 'Inserted ' + data.length + ' new entries in collected table'
+      });
+    }).then(null, function(err) {
+      return next(err);
+    });
+});
+
 
 
 app.post('/products/:id', function(req, res) {
@@ -138,61 +152,72 @@ app.post('/products/:id', function(req, res) {
     return;
   }
   insertOrUpdateEntry(db, req.body)
-      .then(function(data) {
-          res.status(200).json(data);
-      })
-      .then(null, function(err) {
-          return next(err);
-      });
-  });
+    .then(function(data) {
+      res.status(200).json(data);
+    })
+    .then(null, function(err) {
+      return next(err);
+    });
+});
 
+
+function insertList(list) {
+  return db.tx(function(t) {
+    var queries = [];
+    for (i = 0; i < list.length; i++) {
+      var entry = list[i];
+      queries.push(insertEntry(t, entry));
+    }
+    return t.batch(queries);
+  });
+}
 
 function insertOrUpdateEntry(t, entry) {
-    console.log("insertorupdate:");
-    console.log(entry);
-    insertEntry(t, entry)
-        .then(function(result) {
-            // rowCount = number of rows affected by the query
-            return promise.resolve(result);
-        })
-        .then(null, function(error) {
-            return updateEntry(t, entry).then(function(result) {
-                // rowCount = number of rows affected by the query
-                return promise.resolve(result);
-            })
-        });
+  console.log("insertorupdate:");
+  console.log(entry);
+  insertEntry(t, entry)
+    .then(function(result) {
+      // rowCount = number of rows affected by the query
+      return promise.resolve(result);
+    })
+    .then(null, function(error) {
+      return updateEntry(t, entry).then(function(result) {
+        // rowCount = number of rows affected by the query
+        return promise.resolve(result);
+      })
+    });
 }
 
 
 function insertEntry(t, entry) {
-    return t.none(
-      'INSERT into products(barcode, benevning, vendor, vendoritemnumber) ' +
-      'values($1,$2,$3,$4)', [entry.barcode, entry.benevning, entry.vendor, entry.vendoritemnumber]);
+  return t.none(
+    'INSERT into products(barcode, benevning, vendor, vendoritemnumber) ' +
+    'values($1,$2,$3,$4)', [entry.barcode, entry.benevning, entry.vendor, entry.vendoritemnumber]);
 }
 
 function updateEntry(t, entry) {
-    return t.none(
-        'UPDATE products set vendoritemnumber=$2 where barcode=$1', [entry.barcode, entry.vendoritemnumber]);
+  return t.none(
+    'UPDATE products set vendoritemnumber=$2 where barcode=$1', [entry.barcode, entry.vendoritemnumber]);
 }
 
 
 function initializeDb() {
-    console.log("initializing");
-    db.tx(function(t) {
-            return t.batch([
-                t.none('CREATE TABLE IF NOT EXISTS products' +
-                '(barcode text, benevning text, vendor text, vendoritemnumber text,' +
-                ' CONSTRAINT barcodes PRIMARY KEY(barcode))'),
-                t.none('CREATE TABLE IF NOT EXISTS collected(username text, barcode text, info text, collected timestamp)')
-            ]);
-        })
-        .then(function(data) {
-            console.log("db OK");
-        })
-        .then(null, function(error) {
-            console.log("ERROR:", error.message || error);
-            console.log(error)
-        });
+  console.log("initializing");
+  db.tx(function(t) {
+      return t.batch([
+        t.none('CREATE TABLE IF NOT EXISTS products' +
+          '(barcode text, benevning text, vendor text, vendoritemnumber text,' +
+          ' CONSTRAINT barcodes PRIMARY KEY(barcode))'),
+        t.none('CREATE TABLE IF NOT EXISTS collected( id SERIAL UNIQUE, username text, barcode text, info text, collected timestamp)')
+      ]);
+    })
+    .then(function(data) {
+      console.log("db OK");
+    })
+    .then(null, function(error) {
+      console.log("ERROR:", error.message || error);
+      console.log(error)
+    });
 }
 
 initializeDb();
@@ -200,5 +225,5 @@ app.set('port', process.env.PORT || 8080);
 console.log("Trying to listen on " + app.get('port'));
 
 app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
