7de4e060f53760f78b51ffe00dc04d6d898ee3bc
Nils-Morten Nilssen <nmnilssen@gmail.com>
2018-05-04 00:34:15 +0200

    renamed column barcode to streck_kod_lev in the collected table

--- server.js
+++ server.js
@@ -14,12 +14,19 @@ var db = pgp(process.env.DATABASE_URL || "postgres://tov:tov@localhost:5432/tov"
 console.log("Connecting");
 
 var app = express();
-
+/*
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
+*/
+app.use( bodyParser.json({limit: "15360mb", type:'application/json'}) );
+app.use(bodyParser.urlencoded({
+  limit: "15360mb",
+  extended: true,
+  parameterLimit:5000000,
+  type:'application/json'
+}));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -31,7 +38,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/produkter', function(request, response, next) {
-  db.any('SELECT barcode, benevning, vendor, vendoritemnumber FROM products')
+  db.any('SELECT * FROM products')
     .then(function(data) {
       response.render('pages/produkter', {
         results: data
@@ -43,7 +50,7 @@ app.get('/produkter', function(request, response, next) {
 });
 
 app.get('/lister', function(request, response, next) {
-  db.any('SELECT username, barcode, info, collected FROM collected')
+  db.any('SELECT username, streck_kod_lev, info, collected FROM collected')
     .then(function(data) {
       response.render('pages/lists', {
         results: data
@@ -64,23 +71,22 @@ app.all('*', function(req, res, next) {
   next();
 });
 
-app.post('/collect/:id', function(req, res) {
+app.post('/collect/:id', function(req, res,next) {
   var id = req.params.id;
   var user = req.body.user;
-  var barcode = req.body.barcode;
+  var streck_kod_lev = req.body.barcode;
   var info = req.body.info
   console.log("body:" + JSON.stringify(req.body));
   console.log("user:" + user);
   console.log("id:" + id);
-  console.log("barcode:" + barcode);
   console.log("info:" + info);
-  if (!id.localeCompare(barcode) == 0) {
+  if (!id.localeCompare(streck_kod_lev) == 0) {
     console.log("bad id!");
     res.send("Error- bad product id");
     return;
   }
 
-  db.none('INSERT into collected(username, barcode, info, collected) values($1, $2, $3, current_timestamp)', [user, id, info])
+  db.none('INSERT into collected(username, streck_kod_lev, info, collected) values($1, $2, $3, current_timestamp)', [user, id, info])
     .then(function() {
       res.status(200)
         .json({
@@ -107,7 +113,7 @@ app.get('/collected', function(req, res, next) {
 
 
 app.get('/products', function(req, res, next) {
-  db.any('SELECT barcode, benevning, vendor, vendoritemnumber FROM products')
+  db.any('SELECT * FROM products')
     .then(function(data) {
       res.status(200).json(data);
     })
@@ -117,7 +123,7 @@ app.get('/products', function(req, res, next) {
 });
 
 app.get('/products/:id', function(req, res, next) {
-  db.any('SELECT * FROM products where barcode=$1', [req.params.id])
+  db.any('SELECT * FROM products where STRECK_KOD_LEV=$1', [req.params.id])
     .then(function(data) {
       res.status(200).json(data);
     })
@@ -128,11 +134,24 @@ app.get('/products/:id', function(req, res, next) {
 
 app.post('/products', function(req, res, next) {
   console.log("body:" + JSON.stringify(req.body));
+  for (i = 0; i < req.body.length; i++) {
+    var entry = req.body[i];
+    insertOrUpdateEntry(db, entry);
+  }
+  res.status(200)
+    .json({
+      status: 'success',
+      message: 'Inserted new entry in collected table'
+    });
+});
+
+app.post('/products/batch', function(req, res, next) {
+  console.log("body:" + JSON.stringify(req.body));
   insertList(req.body)
     .then(function(data) {
       res.status(200).json({
         status: 'success',
-        message: 'Inserted ' + data.length + ' new entries in collected table'
+        message: 'Inserted or updated ' + req.body.length + ' new entries in collected table'
       });
     }).then(null, function(err) {
       return next(err);
@@ -145,8 +164,8 @@ app.post('/products/:id', function(req, res) {
   var id = req.params.id;
   console.log("post " + id + " has happened :O:O");
   console.log("body:" + JSON.stringify(req.body));
-  var barcode = req.body.barcode;
-  if (!id.localeCompare(barcode) == 0) {
+  var strekkode = req.body.strekkode;
+  if (!id.localeCompare(strekkode) == 0) {
     console.log("bad id!");
     res.send("Error- bad product id");
     return;
@@ -183,30 +202,41 @@ function insertOrUpdateEntry(t, entry) {
         // rowCount = number of rows affected by the query
         return promise.resolve(result);
       })
+    }).catch(function() {
+      console.log("Promise Rejected (insertOrUpdateEntry) - " + entry.strekkode);
     });
 }
 
 
 function insertEntry(t, entry) {
   return t.none(
-    'INSERT into products(barcode, benevning, vendor, vendoritemnumber) ' +
-    'values($1,$2,$3,$4)', [entry.barcode, entry.benevning, entry.vendor, entry.vendoritemnumber]);
+    'INSERT into products(streck_kod_lev, strekkode, benevning, leverandor, levartnr) ' +
+    'values($1,$2,$3,$4,$5)', [entry.streck_kod_lev, entry.strekkode, entry.benevning, entry.leverandor, entry.levartnr]);
 }
 
 function updateEntry(t, entry) {
   return t.none(
-    'UPDATE products set vendoritemnumber=$2 where barcode=$1', [entry.barcode, entry.vendoritemnumber]);
+    'UPDATE products set streck_kod_lev=$2, benevning=$3, leverandor=$4, levartnr=$5 where strekkode=$1', [entry.strekkode, entry.streck_kod_lev, entry.benevning, entry.leverandor, entry.levartnr]);
 }
 
+/*
+{
+  "Strekkode": "8-131-3-25",
+  "LevArtNr": "I-401325",
+  "Benevning": "ICEROSS DERMO LOCKING LINER 3MM, STR 25",
+  "Leverandør": "ØSSUR NORDIC",
+  "STRECK_KOD_LEV": "5690967120528"
+},
+*/
 
 function initializeDb() {
   console.log("initializing");
   db.tx(function(t) {
       return t.batch([
         t.none('CREATE TABLE IF NOT EXISTS products' +
-          '(barcode text, benevning text, vendor text, vendoritemnumber text,' +
-          ' CONSTRAINT barcodes PRIMARY KEY(barcode))'),
-        t.none('CREATE TABLE IF NOT EXISTS collected( id SERIAL UNIQUE, username text, barcode text, info text, collected timestamp)')
+          '(strekkode text, benevning text, leverandor text, levartnr text, streck_kod_lev text,' +
+          ' CONSTRAINT barcodes PRIMARY KEY(strekkode, streck_kod_lev))'),
+        t.none('CREATE TABLE IF NOT EXISTS collected( id SERIAL UNIQUE, username text, streck_kod_lev text, info text, collected timestamp)')
       ]);
     })
     .then(function(data) {
